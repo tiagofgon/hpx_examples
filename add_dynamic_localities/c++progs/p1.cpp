ceb23024f14a86a3df5740465616f6f28ef711ec
tiagofgon <tiago.fg@outlook.com>
2020-10-28 16:17:18 +0000

    .

--- add_dynamic_localities/c++progs/p1.cpp
+++ add_dynamic_localities/c++progs/p1.cpp
@@ -8,31 +8,5 @@ int main() {
 std::cout << "P1 " << std::endl;
 auto res = system("/home/tfg/Documentos/hpx_examples/add_dynamic_localities/build/program --hpx:hpx=localhost:1337 --hpx:expect-connecting-localities --hpx:console");
 
-
-
-//     int pid = fork();
-//     if (pid == 0)
-//     {
-//         execl("/home/tfg/Documentos/hpx_examples/add_dynamic_localities/build/program", "/home/tfg/Documentos/hpx_examples/add_dynamic_localities/build/program", "--hpx:hpx=localhost:1337", "--hpx:expect-connecting-localities", "--hpx:console", "--hpx:localities 2", nullptr);
-//         //execl("/home/tfg/Documentos/hpx_examples/add_dynamic_localities/hello_world/build/program", "/home/tfg/Documentos/hpx_examples/add_dynamic_localities/hello_world/build/program", nullptr);
-
-//     }
-// else if (pid > 0)
-// {
-//     int status;
-//     waitpid(pid, &status, 0);
-
-//     printf("I'm the parent\n");
-//     return 1;
-//     // The parent process, do whatever is needed
-//     // The parent process can even exit while the child process is running, since it's independent
-// }
-// else
-// {
-//     // Error forking, still in parent process (there are no child process at this point)
-//     std::cout << "Fork error:" <<  '\n';
-// }
-
-
     return 0;
 }
